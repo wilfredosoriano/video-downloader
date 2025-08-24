e1ca5a6a948d8ce4a1e25af4a32199e88fd435c8
Wilfredo Jr <macintosh@Wilfredo-Jr-MacBook-Pro.local>
2025-08-25 06:46:35 +0800

    Refactor dependencies to use version catalog

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,7 +41,6 @@ android {
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
@@ -57,22 +56,11 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
-    // Retrofit & Gson
-    implementation("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
-
-    // Lifecycle + Compose integration
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0")
-
-    // Compose Foundation (for layout, Column, Spacer, etc.)
-    implementation("androidx.compose.foundation:foundation")
-
+    implementation(libs.retrofit)
+    implementation(libs.retrofit.converter.gson)
+    implementation(libs.lifecycle.viewmodel.compose)
+    implementation(libs.compose.foundation)
     implementation(libs.androidx.core)
-
-// Kotlin coroutines core
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
-
-// Coroutines support for Android (Dispatchers.Main)
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")
-
+    implementation(libs.coroutines.core)
+    implementation(libs.coroutines.android)
 }
